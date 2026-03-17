f0f5986b70ac743e08302428f926c9d9319bd1aa
z3.o <159318753+z3-o@users.noreply.github.com>
2026-03-17 06:19:33 +0300

    Delete SystemUsesLightTheme.bat

--- SystemUsesLightTheme.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-for /f "tokens=3" %%a in ('reg query "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v SystemUsesLightTheme') do set "x=%%a"
-set /a "num=!x"
-REG ADD "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v SystemUsesLightTheme /t REG_DWORD /d %num% /f
-echo successfully changed SystemUsesLightTheme.
