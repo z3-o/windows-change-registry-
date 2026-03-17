58231ae32097b43aafe4d09047791a0e88d1a8d3
z3.o <159318753+z3-o@users.noreply.github.com>
2026-03-17 05:56:41 +0300

    Add success message to small icon.bat
    Added success message after changing TaskbarSmallIcons.

--- small icon.bat
+++ small icon.bat
@@ -1,6 +1,6 @@
 @echo off
 
-for /f "tokens=3" %%a in ('reg query "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarSmallIcons') do set "x=%%a"
+for /f "tokens=3" %%a in ('reg query "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarSmallIcons') do set "x=%%a"
 set /a "num=!x"
 REG ADD "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarSmallIcons /t REG_DWORD /d %num% /f
 echo successfully changed TaskbarSmallIcons.
